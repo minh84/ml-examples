55f16029ede675cda63e255b14c3196f6d7e7528
Minh VU <ngoc.vu@polytechnique.org>
2017-03-20 12:44:36 +0000

    complete note on intro to nn

--- pysrc/learn_tf/demo_datas.py
+++ pysrc/learn_tf/demo_datas.py
@@ -1,26 +1,60 @@
 import numpy as np
 
 # generate circle-data
-def cicle_data(nb_points=50):
+def circle_data(nb_points=50):
+    '''
+    helper function to generate circle two types of data
+        one in circle center ((0,0), 2) labeled 0
+        one in area of circle center ((0,0),5) - ((0,0),3) labeled 1
+    :param nb_points: an interger controls how many points per label
+    :return:
+            inputs: 2d-points samples
+            labels: points' labels
+    '''
     inputs  = np.zeros([2*nb_points,2])
-    outputs = np.zeros([2*nb_points], dtype=np.int32)
+    labels = np.zeros([2*nb_points], dtype=np.int32)
+
     for i in range(nb_points):
         r,t = np.random.uniform(0., 2.), np.random.uniform(0., 2.*np.pi)
         inputs[i] = [r*np.sin(t), r*np.cos(t)]
         r,t = np.random.uniform(3., 5.), np.random.uniform(0., 2.*np.pi)
         inputs[i+nb_points] = [r*np.sin(t), r*np.cos(t)]
-        outputs[i+nb_points] = 1
+        labels[i+nb_points] = 1
 
-    return inputs, outputs
+    return inputs, labels
 
 # generate 2d-curve
 def curve2d_data(nb_points=50):
+    '''
+    helper function to generate 2 curves one labeled 0, the other labeled 1
+    :param nb_points: nb of points per curve
+    :return:
+            inputs: points sample
+            labels: points' label
+    '''
     x_1 = np.linspace(0., 1.0, nb_points)
     x_2 = np.tanh(10 * (x_1 - 0.5) ** 2)
     inputs = np.zeros([2 * nb_points, 2], dtype=np.float32)
-    outputs = np.zeros([2 * nb_points], dtype=np.int32)
+    labels = np.zeros([2 * nb_points], dtype=np.int32)
     inputs[:, 0] = np.append(x_1, x_1)
     inputs[:, 1] = np.append(x_2 - 0.9, x_2 - 0.1)
-    outputs[nb_points:] = 1
+    labels[nb_points:] = 1
 
-    return  inputs, outputs
+    return  inputs, labels
+
+# generate spiral data
+def spiral_data(nb_points = 100):
+    inputs = np.zeros([2 * nb_points, 2])
+    labels = np.zeros([2 * nb_points], dtype=np.int32)
+
+    for i in range(nb_points):
+        iper = i / nb_points
+        r =  5 * iper + np.random.uniform(-0.1, 0.1)
+        t = 1.25 * iper * 2. * np.pi + np.random.uniform(-0.1, 0.1)
+        inputs[i] = [r * np.sin(t), r * np.cos(t)]
+        r = 5 * iper + np.random.uniform(-0.1, 0.1)
+        t = 1.25 * iper * 2. * np.pi + np.pi + np.random.uniform(-0.1, 0.1)
+        inputs[i + nb_points] = [r * np.sin(t), r * np.cos(t)]
+        labels[i + nb_points] = 1
+
+    return inputs, labels
--- pysrc/learn_tf/nn_fc_layers.py
+++ pysrc/learn_tf/nn_fc_layers.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 class FullyConnectedNN(object):
     def __init__(self, hidden_dims, input_dim = 2, num_classes=2, reg = 0.0, learning_rate = 1e-3,
@@ -14,7 +16,7 @@ class FullyConnectedNN(object):
 
         self.x = tf.placeholder(tf.float32, [None, input_dim])
         self.y = tf.placeholder(tf.int32, [None])
-
+        self.layers = {}
         h = None
         for i in range(len(hidden_dims)):
             with tf.name_scope('hidden_layer_{}'.format(i+1)):
@@ -26,12 +28,15 @@ class FullyConnectedNN(object):
                 else:
                     h = tf.matmul(h, self.params['Wh_{}'.format(i + 1)]) + self.params['bh_{}'.format(i + 1)]
 
+                self.layers['fc_{}'.format(i+1)] = h
                 h = act_fn(h)
+                self.layers['afc_{}'.format(i + 1)] = h
 
         with tf.name_scope('out_layer'):
             self.params['Wo'] = tf.Variable(tf.truncated_normal([dims[-1], num_classes]) * weight_scale, name='Wo')
             self.params['bo'] = tf.Variable(tf.zeros(num_classes), name='bo')
             self.logits = tf.matmul(h, self.params['Wo']) + self.params['bo']
+            self.layers['logits'] = self.logits
 
         with tf.name_scope('train'):
             self.cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y))
@@ -41,7 +46,7 @@ class FullyConnectedNN(object):
             pred = tf.cast(tf.argmax(self.logits, axis=1), tf.int32)
             self.accuracy = tf.reduce_mean(tf.cast(tf.equal(pred, self.y), tf.float32))
 
-    def train(self, inputs, outputs, batch_size, epochs, print_every=20):
+    def train(self, inputs, labels, batch_size, epochs, print_every=20):
         nb_samples = inputs.shape[0]
         iter_per_epoch = nb_samples // batch_size;
         nb_iters = iter_per_epoch * epochs
@@ -51,10 +56,10 @@ class FullyConnectedNN(object):
             for i in range(nb_iters):
                 idx = np.random.choice(nb_samples, batch_size)
                 _ = sess.run(self.train_op,
-                             feed_dict={self.x : inputs[idx], self.y : outputs[idx]})
+                             feed_dict={self.x : inputs[idx], self.y : labels[idx]})
                 if (i%print_every==0 or i+1==nb_iters):
                     loss, acc = sess.run([self.cost, self.accuracy],
-                                         feed_dict={self.x:inputs, self.y:outputs})
+                                         feed_dict={self.x : inputs, self.y : labels})
                     print('{:>5d} iter loss = {:>10.4f} acc = {:>5.2f}'.format(i, loss, acc*100.))
 
             for k in self.params:
@@ -62,4 +67,44 @@ class FullyConnectedNN(object):
 
         return fitted_params
 
+    def forward(self, sess, layer, inputs):
+        return sess.run(self.layers[layer], feed_dict = {self.x : inputs})
 
+    def visualize(self, layer, fitted_params, inputs, labels, cut_params = None):
+        with tf.Session() as sess:
+            # assign fitted weights & biases
+            for k in fitted_params:
+                sess.run(tf.assign(self.params[k], fitted_params[k]))
+
+            # forward pass to layer that we want to visualize
+            outputs = self.forward(sess, layer, inputs)
+
+            # visualize layer if it 2D/3D
+            colors = np.array(['b']*len(labels))
+            colors[labels==1]= 'r'
+            if (outputs.shape[1] == 2):
+                ax = plt.subplot()
+                ax.scatter(outputs[:, 0], outputs[:,1], c = colors)
+                if cut_params is not None:
+                    cut_w = cut_params['w'][:, 1] - cut_params['w'][:, 0]
+                    cut_b = cut_params['b'][1] - cut_params['b'][0]
+                    x = np.linspace(np.min(outputs[:,0]), np.max(outputs[:,0]), 20)
+                    y = (-cut_b - cut_w[0] * x )/cut_w[1]
+                    ax.plot(x, y, c='black')
+
+            elif (outputs.shape[1] == 3):
+                ax = plt.subplot(projection='3d')
+                ax.view_init(180, 50)
+                ax.scatter(outputs[:, 0], outputs[:,1], outputs[:,2], c = colors)
+
+                if cut_params is not None:
+                    cut_w = cut_params['w'][:, 1] - cut_params['w'][:, 0]
+                    cut_b = cut_params['b'][1] - cut_params['b'][0]
+                    x = np.linspace(np.min(outputs[:, 0]), np.max(outputs[:, 0]), 10)
+                    y = np.linspace(np.min(outputs[:, 1]), np.max(outputs[:, 1]), 10)
+                    X, Y = np.meshgrid(x, y)
+                    Z = - (cut_w[0] * X + cut_w[1] * Y + cut_b)/cut_w[2]
+                    ax.plot_surface(X, Y, Z, alpha=0.5)
+
+            else:
+                raise Exception('visualize currently supports only 2D or 3D output')
