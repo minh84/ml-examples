05e7d2fa9643aec1ae97ce837fe82fc839d90f33
Minh VU <ngoc.vu@polytechnique.org>
2017-04-21 21:53:15 +0100

    add traning word2vec, rename object to make it supports both neg/nce loss-function and log-uni/neg sampling

--- pysrc/common/nlp/word2vec.py
+++ pysrc/common/nlp/word2vec.py
@@ -13,17 +13,13 @@ def get_target(words, idx, window_size=5):
     return list(target_words)
 
 
-class Word2vecNeg(object):
+class Word2vecSampling(object):
     def __init__(self,
                  vocabs,
                  word2id,
                  id2word,
                  freqs,
-                 train_wordids,
-                 settings = {'embed_dim'     : 200,
-                             'batch_size'    : 16,
-                             'nb_neg_sample' : 100,
-                             'window_size'   : 5}):
+                 train_wordids):
         self._vocabs = vocabs
         self._word2id = word2id
         self._id2word = id2word
@@ -35,54 +31,6 @@ class Word2vecNeg(object):
         self._freqs_list = [0.] * self._vocabs_size
         for w in self._vocabs:
             self._freqs_list[self._word2id[w]] = self._freqs[w]
-
-        # get hyper-parameters
-        self._embed_dim     = settings.get('embed_dim',     200)
-        self._batch_size    = settings.get('batch_size',    16)
-        self._nb_neg_sample = settings.get('nb_neg_sample', 100)
-        self._window_size   = settings.get('window_size',   5)
-
-
-    #     self._neg_table_size = settings.get('neg_table_size', 1000000)
-    #     self.negSampleTable()
-    #
-    # def negSampleTable(self, table_size = None):
-    #     '''
-    #     we build a sample table st where
-    #         number of i such that st[i] = w is freq[w]
-    #     this sample-table allow us to quickly do the nagative-sampling in the cost of memory
-    #     :param table_size: size of sample-table
-    #     :return: a sample table
-    #     '''
-    #     if table_size is None:
-    #         table_size = self._neg_table_size
-    #
-    #     if self._sampleTable is None or len(self._sampleTable) != table_size:
-    #         nTokens = len(self._id2word)
-    #         samplingFreq = np.zeros((nTokens,))
-    #
-    #         for i,w in self._id2word.items():
-    #             if w in self._freqs:
-    #                 freq = self._freqs[w] ** 0.75
-    #             else:
-    #                 freq = 0.0
-    #             samplingFreq[i] = freq
-    #
-    #         samplingFreq /= np.sum(samplingFreq)
-    #         samplingFreq = np.cumsum(samplingFreq) * table_size
-    #
-    #         self._sampleTable = [0] * table_size
-    #
-    #         j = 0
-    #         for i in range(table_size):
-    #             while i > samplingFreq[j]:
-    #                 j += 1
-    #             self._sampleTable[i] = j
-    #
-    #     return self._sampleTable
-    #
-    # def negSampleIdx(self):
-    #     return self.negSampleTable()[np.random.randint(0, self._neg_table_size-1)]
 
     def _create_placeholder(self):
         with self._graph.as_default():
@@ -108,29 +56,57 @@ class Word2vecNeg(object):
             labels_matrix = tf.cast(self._target_words, dtype=tf.int64)
 
             # negative sampling.
-            sampled_values = (tf.nn.fixed_unigram_candidate_sampler(
-                true_classes=labels_matrix,
-                num_true=1,
-                num_sampled=self._nb_neg_sample,
-                unique=True,
-                range_max=self._vocabs_size,
-                distortion=0.75,
-                unigrams=self._freqs_list))
+            sampled_values = None # default it will use log_uni
+            if self._sampling_method == 'neg':
+                sampled_values = (tf.nn.fixed_unigram_candidate_sampler(
+                    true_classes=labels_matrix,
+                    num_true=1,
+                    num_sampled=self._nb_neg_sample,
+                    unique=True,
+                    range_max=self._vocabs_size,
+                    distortion=0.75,
+                    unigrams=self._freqs_list))
 
             # compute loss
-            self._loss = tf.nn.sampled_softmax_loss(self._softmax_weights, self._softmax_biases,
-                                                    self._target_words, self._embed,
-                                                    self._nb_neg_sample, self._vocabs_size,
-                                                    sampled_values=sampled_values)
+            if self._loss_func == 'neg':
+                self._loss = tf.nn.sampled_softmax_loss(self._softmax_weights, self._softmax_biases,
+                                                        self._target_words, self._embed,
+                                                        self._nb_neg_sample, self._vocabs_size,
+                                                        sampled_values=sampled_values)
+            elif self._loss_func == 'nce':
+                self._loss = tf.nn.nce_loss(self._softmax_weights, self._softmax_biases,
+                                            self._target_words, self._embed,
+                                            self._nb_neg_sample, self._vocabs_size,
+                                            sampled_values=sampled_values)
+            else:
+                raise Exception('Unknown sampling method {}, currently only support neg/nce'.format(self._sampling_method))
 
             # compute cost
             self._cost = tf.reduce_mean(self._loss)
 
-            # creat optimizer
-            self._optimizer = tf.train.AdamOptimizer().minimize(self._cost)
+            # create global_step to store training-step
+            self._global_step = tf.Variable(0, dtype=tf.int32,
+                                            trainable=False, name='global_step')
+
+            # creat optimizer and pass the global_step to make it incremented each traingin-step
+            self._optimizer = tf.train.AdamOptimizer(learning_rate=self._learning_rate).minimize(self._cost,
+                                                                                                 global_step=self._global_step)
 
 
-    def build_graph(self):
+    def build_graph(self,settings = {'embed_dim'       : 200,
+                                     'nb_neg_sample'   : 100,
+                                     'learning_rate'   : 0.2,
+                                     'sampling_method' : 'neg',
+                                     'loss_func'       : 'nce'}):
+        # get hyper-parameters
+        self._embed_dim         = settings.get('embed_dim', 200)
+        self._nb_neg_sample     = settings.get('nb_neg_sample', 100)
+        self._learning_rate     = settings.get('learning_rate', 0.2)
+        self._sampling_method   = settings.get('sampling_method', 'neg')
+        self._loss_func         = settings.get('loss_func', 'nce')
+        assert (self._sampling_method == 'neg' or self._sampling_method == 'log_uni')
+        assert (self._loss_func == 'neg' or self._loss_func == 'nce')
+
         self._graph = tf.Graph()
 
         # create input/output placeholder
@@ -142,32 +118,39 @@ class Word2vecNeg(object):
         # create neg-sampling loss & optimizer
         self._create_neg_sampling_loss()
 
-    def get_batches(self):
-        n_batches = len(self._train_wordids) // self._batch_size
+        # create saver to save
+        with self._graph.as_default():
+            self._saver = tf.train.Saver()
+
+    def get_batches(self, batch_size, window_size):
+        n_batches = len(self._train_wordids) // batch_size
 
         # only full batches
-        words = self._train_wordids[:n_batches * self._batch_size]
+        words = self._train_wordids[:n_batches * batch_size]
 
-        for idx in range(0, len(words), self._batch_size):
+        for idx in range(0, len(words), batch_size):
             x, y = [], []
-            batch = words[idx:idx + self._batch_size]
+            batch = words[idx:idx + batch_size]
             for ii in range(len(batch)):
                 batch_x = batch[ii]
-                batch_y = get_target(batch, ii, self._window_size)
+                batch_y = get_target(batch, ii, window_size)
                 y.extend(batch_y)
                 x.extend([batch_x] * len(batch_y))
             yield x, y
 
-    def train(self):
-        epochs = 5
+    def train(self, epochs, batch_size, window_size,
+                    print_every = 100, save_every=1000):
         iteration = 1
+        n_batches = len(self._train_wordids) // batch_size
+
         with tf.Session(graph=self._graph) as sess:
             sess.run(tf.global_variables_initializer())
 
             loss = 0
             for e in range(1, epochs + 1):
-                batches = self.get_batches()
+                batches = self.get_batches(batch_size, window_size)
                 start = time()
+                i_batch = 1
                 for x, y in batches:
                     feed = {self._center_words : x,
                             self._target_words : np.array(y)[:, None]}
@@ -176,13 +159,20 @@ class Word2vecNeg(object):
 
                     loss += train_loss
 
-                    if iteration % 100 == 0:
+                    if iteration % print_every == 0:
                         end = time()
-                        print("Epoch {}/{}".format(e, epochs),
-                              "Iteration: {}".format(iteration),
+                        print("Epoch ({}/{})".format(e, epochs),
+                              "Batch ({:>5d}/{:<5d})".format(i_batch, n_batches),
+                              "Iteration: {:>8d}".format(iteration),
                               "Avg. Training loss: {:.4f}".format(loss / 100),
                               "{:.4f} sec/batch".format((end - start) / 100))
                         loss = 0
                         start = time()
 
+                    if iteration % save_every == 0:
+                        self._saver.save(sess,
+                                         'checkpoints/skip-gram',
+                                         global_step=self._global_step)
+
+                    i_batch += 1
                     iteration+=1
